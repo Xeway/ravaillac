277abadc05d2e0e638636cf7e6258064eafa0a3c
xeway <xeway@protonmail.com>
2022-11-28 21:17:28 +0100

    CReated folder to put files

--- ravaillac.py
+++ ravaillac.py
@@ -4,13 +4,22 @@ import math
 def ravaillac(file_name, size_limit):
     # with open(file_name, "rb") as file:
     #     print(file.read())
-    file_size = os.stat(file_name).st_size
+    if size_limit <= 0:
+        return
 
+    file_size = os.stat(file_name).st_size
     if file_size <= size_limit:
         return
 
     splits = math.ceil(file_size/size_limit)
 
-    print(splits)
+    folder_id = 0
+    result_path = f"ravaillac_result/{os.path.basename(file_name)}_{int(size_limit)}B"
+    while os.path.exists(result_path):
+        result_path = f"ravaillac_result/{os.path.basename(file_name)}_{int(size_limit)}B({folder_id})"
+        folder_id += 1
 
-ravaillac("./test_files/debian-11.5.0-amd64-netinst.iso", 8e6)
+    os.makedirs(result_path)
+
+
+ravaillac("test_files/debian-11.5.0-amd64-netinst.iso", 8e6)
