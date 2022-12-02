3360f00e330585375717ca9c158e8a10a9d6114a
xeway <xeway@protonmail.com>
2022-12-02 23:01:42 +0100

    merge_fragments created

--- ravaillac.py
+++ ravaillac.py
@@ -2,7 +2,7 @@ import os
 import math
 
 
-def ravaillac(file_name, size_limit):
+def split_file(file_name, size_limit):
     if size_limit <= 0:
         return
 
@@ -14,9 +14,9 @@ def ravaillac(file_name, size_limit):
     file_name_without_extension, file_extension = os.path.splitext(only_file_name)
 
     folder_id = 1
-    result_path = f"ravaillac_result/{only_file_name}_{int(size_limit)}B"
+    result_path = f"ravaillac_result/splits/{only_file_name}_{int(size_limit)}B"
     while os.path.exists(result_path):
-        result_path = f"ravaillac_result/{only_file_name}_{int(size_limit)}B-{folder_id}"
+        result_path = f"ravaillac_result/splits/{only_file_name}_{int(size_limit)}B-{folder_id}"
         folder_id += 1
 
     os.makedirs(result_path)
@@ -31,9 +31,9 @@ def ravaillac(file_name, size_limit):
     i = 0
     fragment_number = 1
     while i <= len(file_value) - fragment_size:
-        with open(f"{result_path}/{file_name_without_extension}_fragment{fragment_number}{file_extension}",
-                  "wb") as file:
-            file.write(file_value[i:i + fragment_size])
+        with open(f"{result_path}/{file_name_without_extension}--fragment{fragment_number}{file_extension}",
+                  "wb") as fragment:
+            fragment.write(file_value[i:i + fragment_size])
 
         i += fragment_size
         fragment_number += 1
@@ -41,9 +41,26 @@ def ravaillac(file_name, size_limit):
     # we treat the rest
     # <=> if file_size % splits > 0
     if i != len(file_value):
-        with open(f"{result_path}/{file_name_without_extension}_fragment{fragment_number}{file_extension}",
-                  "wb") as file:
-            file.write(file_value[i:len(file_value)])
+        with open(f"{result_path}/{file_name_without_extension}--fragment{fragment_number}{file_extension}",
+                  "wb") as fragment:
+            fragment.write(file_value[i:len(file_value)])
+
+    return result_path
 
 
-ravaillac("test_files/debian-11.5.0-amd64-netinst.iso", 8e6)
+def merge_fragments(path):
+    fragments = os.listdir(path)
+    fragments.sort(key=lambda x: int(x.split("--fragment", 1)[1].split(".", 1)[0]))
+
+    for fragment_name in fragments:
+        with open(os.path.join(path, fragment_name), "rb") as fragment:
+            fragment_value = fragment.read()
+
+        fragment_name_without_extension, fragment_extension = os.path.splitext(fragment_name)
+
+        with open(fragment_name_without_extension.split("--fragment", 1)[0] + fragment_extension, "ab") as original_file:
+            original_file.write(fragment_value)
+
+# split_file("test_files/debian-11.5.0-amd64-netinst.iso", 8e6)
+
+merge_fragments("ravaillac_result/splits/debian-11.5.0-amd64-netinst.iso_8000000B")
