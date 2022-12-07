71e50b9258f54765172212dcb8350f5fcbc70d03
xeway <xeway@protonmail.com>
2022-12-07 22:07:46 +0100

    Created very basic GUI for file splitting

--- gui.py
+++ gui.py
@@ -1,8 +1,19 @@
 import tkinter as tk
+from tkinter import filedialog
+
+def openFile():
+    file_path = filedialog.askopenfilename()
+    print(file_path)
+
 
 root = tk.Tk()
 
 root.geometry("500x600")
 root.title("Ravaillac")
+
+label = tk.Label(root, text="Choose a file to split")
+label.pack()
+button = tk.Button(text="Open", command=openFile)
+button.pack()
 
 root.mainloop()
