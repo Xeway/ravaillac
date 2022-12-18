cf1787ba70131cf4e1e93de54b188f3ccd5683a6
xeway <xeway@protonmail.com>
2022-12-18 20:22:20 +0100

    Modern GUI and tabs

--- gui.py
+++ gui.py
@@ -1,19 +1,27 @@
-import tkinter as tk
 from tkinter import filedialog
+import customtkinter
 
-def openFile():
+
+def open_file():
     file_path = filedialog.askopenfilename()
     print(file_path)
 
 
-root = tk.Tk()
+customtkinter.set_appearance_mode("dark")
+
+root = customtkinter.CTk()
 
 root.geometry("500x600")
 root.title("Ravaillac")
 
-label = tk.Label(root, text="Choose a file to split")
+tabview = customtkinter.CTkTabview(root, width=450, height=580)
+tabview.pack()
+split_tab = tabview.add("Split a file")
+merge_tab = tabview.add("Merge fragments")
+
+label = customtkinter.CTkLabel(master=split_tab, text="Choose a file to split")
 label.pack()
-button = tk.Button(text="Open", command=openFile)
+button = customtkinter.CTkButton(master=split_tab, text="Open", command=open_file)
 button.pack()
 
 root.mainloop()
